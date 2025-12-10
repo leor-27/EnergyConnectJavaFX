c5602156503a164f9b7fef066123513273c9439e
leor-27 <leoangelolarreagpoon@gmail.com>
2025-12-10 21:33:22 +0800

    Add files via upload

---
+++ main/java/com/example/energyconnectjavafx/Controller/AboutController.java
@@ -0,0 +1,24 @@
+package com.example.energyconnectjavafx.Controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.web.WebView;
+import javafx.event.Event;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+public class AboutController extends NavController {
+//    @FXML
+//    private WebView mapView;
+
+//    public void initialize() {
+//
+//        mapView.getEngine().setJavaScriptEnabled(true);
+//        mapView.getEngine().load(
+//                getClass().getResource("/com/example/energyconnectjavafx/map.html").toExternalForm()
+//        );
+//    }
+}
---
+++ main/java/com/example/energyconnectjavafx/Controller/NewsController.java
@@ -0,0 +1,16 @@
+package com.example.energyconnectjavafx.Controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
+
+public class NewsController extends NavController {
+
+    @FXML
+    public ImageView logo;
+    @FXML
+    private StackPane headerContainer;
+    @FXML
+    private ImageView headerBackground;
+
+}
